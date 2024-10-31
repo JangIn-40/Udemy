523ef0445cb526b1ba3994a2eca8f127267df7b6
JangIn-40 <jih105802@gmail.com>
2024-10-31 22:31:18 +0900

    Test Behavior Tree
    행동트리 맛보기! 재밌다 ㅎㅎ

---
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	//if (LineOfSightTo(PlayerPawn))
+	//{
+	//	MoveToActor(PlayerPawn, AcceptanceRadius);
+	//	SetFocus(PlayerPawn);
+	//}
+	//else
+	//{
+	//	ClearFocus(EAIFocusPriority::Gameplay);
+	//	StopMovement();
+	//}
+}
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AIBehavior)
+	{
+		RunBehaviorTree(AIBehavior);
+
+		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+	}
+}
